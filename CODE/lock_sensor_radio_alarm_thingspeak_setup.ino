e768226982063daa7bbcc5a4ffbe0e7538d7b285
jarlp <64584185+jarlp@users.noreply.github.com>
2025-01-21 12:51:43 +0100

    change delay in lock, so its not dependent on thingspeak

--- CODE/lock_sensor_radio_alarm_thingspeak_setup.ino
+++ CODE/lock_sensor_radio_alarm_thingspeak_setup.ino
@@ -3,7 +3,6 @@
 #include "Servo.h"
 #include <ESP8266WiFi.h>
 #include <ThingSpeak.h>
-#include <LiquidCrystal_I2C.h>
 
 
 //variables
@@ -19,19 +18,14 @@ const char* password = "detvedjegikke";
 WiFiClient client; 
 unsigned long channelID = 2814485;
 const char* APIKey = "6COFCODQXCAV6F9O";
-const int postDelay = 20 * 1000; // 20-second delay
+// const int postDelay = 20 * 1000; // 20-second delay
 
 Servo lock;  
 // Create Amplitude Shift Keying Object
-RH_ASK rf_driver(2000,D4);
-
-// set the LCD number of columns and rows
-int lcdColumns = 16;
-int lcdRows = 2;
+RH_ASK rf_driver(2000,2);
 
 // set LCD address, number of columns and rows
 // if you don't know your display address, run an I2C scanner sketch
-LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);  
 
 void setup() {
   rf_driver.init();      // Initialize receiver
@@ -52,21 +46,16 @@ void setup() {
 
   // Initialize ThingSpeak
   ThingSpeak.begin(client);
-
-    // initialize LCD
-  lcd.init();
-  // turn on LCD backlight                      
-  lcd.backlight();
 }
 
 
+unsigned long previousMillis = 0;
+const unsigned long postDelay = 20000; // 20 seconds
 
 void loop() {
-  
-  // set cursor to first column, first row
-  lcd.setCursor(0, 0);
-  // clears the display to print new message
-  lcd.clear();
+
+ unsigned long currentMillis = millis();
+
 
   uint8_t buf[25 + 1];
   uint8_t buflen = sizeof(buf);
@@ -78,54 +67,57 @@ void loop() {
         
         door=0;
 
-        lcd.print("Locking the door");
+         Serial.println("Locking the door");
         lock.write(160);
       }
       else if(strcmp((char*)buf , "LockedNClose") == 0){
-        lcd.print("Locking the door223");
+         Serial.println("Locking the door223");
               door=0;
               
               alarm = 0;
         for (int i=0; i<10; i++)   //The alarm goes off for 10 seconds
               alarm = 1;
-        lcd.print("Alarm goes off");
+         Serial.println("Alarm goes off");
       
         lock.write(160);
       }
       else if(strcmp((char* )buf , "UnlockNFar") == 0){
-        lcd.print("Unlocking the door");
+         Serial.println("Unlocking the door");
         
         door=1;
         
         lock.write(0);
       }
       else if(strcmp((char* )buf , "UnlockNClose") == 0){
-        lcd.print("Unlocking the door");
+         Serial.println("Unlocking the door");
         
         door=1;
 
         lock.write(0);
       }
       else{
-        lcd.print("Message Received: ");
-        lcd.print((char*)buf);
+
+        Serial.print("Message Received: ");
+        Serial.print((char*)buf);
       }
 
     }
 
-    // Send data to ThingSpeak
-    ThingSpeak.setField(1, door);
-    ThingSpeak.setField(2, alarm);
-    //ThingSpeak.setField(3, light);
-    
-    int response = ThingSpeak.writeFields(channelID, APIKey);
-    if (response == 200) {
-      lcd.print("/nData sent to ThingSpeak successfully!");
-    } else {
-      lcd.print("Error sending data to ThingSpeak: ");
-      lcd.print(response);
+ // Message handling logic here
+
+    if (currentMillis - previousMillis >= postDelay) {
+        previousMillis = currentMillis;
+
+        // Send data to ThingSpeak
+        ThingSpeak.setField(1, door);
+        ThingSpeak.setField(2, alarm);
+        int response = ThingSpeak.writeFields(channelID, APIKey);
+
+        if (response == 200) {
+            Serial.println("Data sent to ThingSpeak successfully!");
+        } else {
+            Serial.print("Error sending data to ThingSpeak: ");
+            Serial.println(response);
+        }
     }
-
-    delay(postDelay); // Wait before sending the next update
-
 }
